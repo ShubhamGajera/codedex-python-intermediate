036f7f6c4c0ec2a14866f0c18e598a36c07aad30
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-09-04 22:13:59 +0530

    04 Fruit Cart - codedex github

--- codedex-1.py
+++ codedex-1.py
@@ -102,3 +102,25 @@ set_difference=set1.difference(set2)
 print(set_union)
 print(set_intersection)
 print(set_difference)
+
+
+# codedex github
+
+# Fruit Cart 🍓
+# Codédex
+
+my_fruits = {'apple', 'banana', 'kiwi'}
+friend_fruits = {'banana', 'orange', 'grape'}
+
+# Uncomment to check if a fruit is <in> both list
+# print('apple' in fruits)  # Output: True
+# set3 = {'apple', 'orange'}
+# print(set3.issubset(fruits))  # Output: True
+
+union_result = my_fruits | friend_fruits
+intersection_result = my_fruits & friend_fruits
+difference_result = my_fruits - friend_fruits
+
+print('Union:', union_result)
+print('Intersection:', intersection_result)
+print("My Fruits - Friend's Fruits:", difference_result)
